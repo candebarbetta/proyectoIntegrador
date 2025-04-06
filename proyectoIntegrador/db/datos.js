db9105078606dabf304b93162e6ed52c3937e70b
candebarbetta <cbarbetta@udesa.edu.ar>
2025-04-06 12:49:33 -0300

    hice el punto 2

---
+++ proyectoIntegrador/db/datos.js
@@ -0,0 +1,177 @@
+const data= {
+    usuario: {
+        email: "bernigamboa@gmail.com",
+        usuario: "berni123",
+        contrasena: "candelaria02",
+        fechaNacimiento: "03-03-2006",
+        documento: 46751724,
+        fotoPerfil: "/images/user/default-image.png"
+      },
+      productos: [
+        {
+          imagen: "/images/products/img-cafetera-moulinex,jpg",
+          nombre: "Cafetera",
+          descripcion: "Cafetera de oficina",
+          comentarios: [
+            {
+              nombreUsuario: "bernixd",
+              texto: "Que rico cafe!",
+              imagenPerfil: "/images/user/default-image.png"
+            },
+            {
+              nombreUsuario: "candelol",
+              texto: "La uso todos los días, 10/10!!",
+              imagenPerfil: "/images/user/default-image.png"
+            }
+          ]
+        },
+        {
+          imagen: "/images/products/img-mackbook-pro-2019.jpg",
+          nombre: "Computadora Mackbook",
+          descripcion: "Ideal para estudiantes y home office",
+          comentarios: [
+            {
+                nombreUsuario: "bernixd",
+                texto: "Que buena compu!",
+                imagenPerfil: "/images/user/default-image.png"
+              },
+              {
+                nombreUsuario: "candelol",
+                texto: "La uso todos los días, 10/10!!",
+                imagenPerfil: "/images/user/default-image.png"
+              }
+          ]
+        },
+        {
+          imagen: "/images/products/img-samsung-galaxy-s10.jpg",
+          nombre: "Samsung galaxy s10",
+          descripcion: "Buena calidad de sonido y duración de batería.",
+          comentarios: [
+            {
+              nombreUsuario: "maria123",
+              texto: "Funciona perfecto",
+              imagenPerfil: "/images/user/default-image.png"
+            }
+          ]
+        },
+        {
+          imagen: "/images/products/img-tv-samsung-smart.jpg",
+          nombre: "Television samsung",
+          descripcion: "Television de muy buena calidad",
+          comentarios: [
+            {
+                nombreUsuario: "bernixd",
+                texto: "Que buena tele!",
+                imagenPerfil: "/images/user/default-image.png"
+              },
+              {
+                nombreUsuario: "candelol",
+                texto: "La uso todos los días, 10/10!!",
+                imagenPerfil: "/images/user/default-image.png"
+              }
+          ]
+        },
+        {
+    
+          imagen: "/images/products/gatos.png",
+          nombre: "Gato marron",
+          descripcion: "Muy amigable",
+          comentarios: [
+            {
+                nombreUsuario: "bernixd",
+                texto: "Que buen gato!",
+                imagenPerfil: "/images/user/default-image.png"
+              },
+              {
+                nombreUsuario: "candelol",
+                texto: "Lo amo",
+                imagenPerfil: "/images/user/default-image.png"
+              }
+          ]
+        },
+        {
+          imagen: "/images/products/reloj.jpeg",
+          nombre: "Reoj rolex",
+          descripcion: "Muy buena hora",
+          comentarios: [            {
+            nombreUsuario: "bernixd",
+            texto: "Que buen reloj!",
+            imagenPerfil: "/images/user/default-image.png"
+          },
+          {
+            nombreUsuario: "candelol",
+            texto: "Lo uso todos los días, 10/10!!",
+            imagenPerfil: "/images/user/default-image.png"
+          }]
+        },
+        {
+          imagen: "/images/products/zapaptillas.webp",
+          nombre: "Zapatillas samba verdes",
+          descripcion: "De buena calidad",
+          comentarios: [
+            {
+                nombreUsuario: "bernixd",
+                texto: "Que buenas zapas!",
+                imagenPerfil: "/images/user/default-image.png"
+              },
+              {
+                nombreUsuario: "candelol",
+                texto: "Las uso todos los días, 10/10!!",
+                imagenPerfil: "/images/user/default-image.png"
+              }
+          ]
+        },
+        {
+          imagen: "/images/products/silla.webp",
+          nombre: "Silla de madera",
+          descripcion: "Perfecta para el comedor",
+          comentarios: [
+            {
+                nombreUsuario: "bernixd",
+                texto: "Que buena silla!",
+                imagenPerfil: "/images/user/default-image.png"
+              },
+              {
+                nombreUsuario: "candelol",
+                texto: "La uso todos los días, 10/10!!",
+                imagenPerfil: "/images/user/default-image.png"
+              }
+          ]
+        },
+        {
+          imagen: "/images/products/images.jpeg",
+          nombre: "Cartera Prune",
+          descripcion: "Incluye 36 colores brillantes.",
+          comentarios: [
+            {
+                nombreUsuario: "bernixd",
+                texto: "Que buena cartera!",
+                imagenPerfil: "/images/user/default-image.png"
+              },
+              {
+                nombreUsuario: "candelol",
+                texto: "La uso todos los días, 10/10!!",
+                imagenPerfil: "/images/user/default-image.png"
+              }
+          ]
+        },
+        {
+          imagen: "/images/products/sweater.jpeg",
+          nombre: "sweater de zara",
+          descripcion: "Ideal para viajes y días largos.",
+          comentarios: [
+            {
+                nombreUsuario: "bernixd",
+                texto: "Que buen sweater!",
+                imagenPerfil: "/images/user/default-image.png"
+              },
+              {
+                nombreUsuario: "candelol",
+                texto: "Lo uso todos los días, 10/10!!",
+                imagenPerfil: "/images/user/default-image.png"
+              }
+          ]
+        }
+      ]
+    };
+    
