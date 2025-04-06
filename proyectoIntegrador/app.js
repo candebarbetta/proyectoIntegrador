17f3c228cc19dbf5324545087be231ab2c77ebc1
BernardaGamboa <mgamboacarrica@udesa.edu.ar>
2025-04-06 11:35:14 -0300

    hice el forms de login y renderice su vista

--- proyectoIntegrador/app.js
+++ proyectoIntegrador/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const registroRouter = require('./routes/registro')
+const loginRouter = require('./routes/login')
 
 var app = express();
 
@@ -22,7 +23,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/registro', registroRouter)
+app.use('/registro', registroRouter);
+app.use('/login', loginRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
