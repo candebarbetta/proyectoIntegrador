16fdb4e47b4e5d95671159cc1eaf7364fd084df9
BernardaGamboa <mgamboacarrica@udesa.edu.ar>
2025-05-28 17:03:17 -0300

    hice el punto 7

--- proyectoIntegrador/controllers/indexController.js
+++ proyectoIntegrador/controllers/indexController.js
@@ -2,18 +2,22 @@ const db = require("../database/models");
 
 const indexController = {
   productos: function (req, res) {
-    db.Producto.findAll()
-      .then(function (productos) {
-        res.render("index", {
-          data: {
-            productos,
-          },
-        });
-      })
-      .catch(function (error) {
-        console.error("ERROR EN LA CONSULTA A PRODUCTOS:", error);
-        res.send("Error al cargar productos");
+    db.Producto.findAll({
+      include: [
+        { association: 'usuario' }
+      ]
+    })
+    .then(function (productos) {
+      res.render("index", {
+        data: {
+          productos: productos,
+        },
       });
+    })
+    .catch(function (error) {
+      console.error("Error", error);
+      res.send("Error al cargar productos");
+    });
   },
 };
 
